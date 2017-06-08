e869b2fc819aeb32b941ae01c2c0cd34cba662d6
KNOXDEV <knox@doot.pw>
2017-06-07 19:12:07 -0700

    Created the central Feather class. Will be updated much more in the future.

---
+++ src/pw/knx/feather/Feather.java
@@ -0,0 +1,17 @@
+package pw.knx.feather;
+
+/**
+ * The central class of the Feather library.
+ *
+ * Basically functions as a convenient state manager for OpenGL,
+ * wrapping the most commonly used functions in intuitive, intelligent,
+ * chainable methods. In this way, Feather reduces your required knowledge of
+ * obscure OpenGL constants.
+ *
+ * WIP
+ *
+ * @author KNOXDEV
+ * @since 6/7/2017 6:54 PM
+ */
+public class Feather {
+}
